6859298df143be35bf09a7ff0a6f6c1c034842c2
Walter Mejía <waltermejia61@hotmail.com>
2023-05-08 12:15:26 -0600

    Agregado module

---
+++ src/app/cruds/presupuesto/presupuesto-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+
+const routes: Routes = [
+  { path: "", redirectTo: 'list', pathMatch: 'full', },
+  { path: "list", component: ListComponent },
+  // { path: "new", component: NewComponent },
+  // { path: "edit/:id", component: NewComponent },
+  // { path: "evaluate/:id", component: EvaluateComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PresupuestoRoutingModule { }
