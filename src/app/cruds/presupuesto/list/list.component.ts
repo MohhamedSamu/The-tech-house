b07843bda83759ca66aaeef010356bb34f999536
Walter Mejía <waltermejia61@hotmail.com>
2023-05-08 16:15:24 -0600

    Avance

--- src/app/cruds/presupuesto/list/list.component.ts
+++ src/app/cruds/presupuesto/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  projects: any[] = [];
 
+  constructor(
+    private router: Router,
+    ){}
+
+    ngOnInit(){
+      // this.projects = this.projectService.getProjects();
+    }
+
+    
 }
